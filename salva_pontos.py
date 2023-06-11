b2096cab5238d0747e0f5124bfd8a71c394b9447
Pedro Henrique <63562843+pedrocorrea2002@users.noreply.github.com>
2023-06-11 20:53:50 -0300

    Voltando a pegar as amostras também de cabeça para baixo

--- salva_pontos.py
+++ salva_pontos.py
@@ -27,7 +27,7 @@ sequence_length = 30 #Números de frames por amostra
 
 # Criando pastas
 for action in actions:
-    for sequence in range(int(nro_amostra_inicial),(int(nro_amostra_inicial) + int(no_sequences))*2):
+    for sequence in range(int(nro_amostra_inicial),(int(nro_amostra_inicial) + int(no_sequences))*4):
         try:
             os.makedirs(os.path.join(DATA_PATH,action, str(sequence)))
         except:
@@ -41,89 +41,101 @@ cv2.resizeWindow("Captura", 1080, 720)
 # Acessando modelo Holistic do mediapipe
 # Testado outros valores para o min_detection_confidence e min_tracking_confidence, melhor deixar 0.5 para os 2 por hora
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    #Se eu uso o mesmo holistic para imagens normais e para imagens invertidas, o holistic se perde, porque ele fica tentando acompanhar seu movimento e acaba passando por duas imagens totalmente diferentes
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_rh_v:
+        with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_h_rv:
+            with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic_rh_rv:
 
-    # Loop de sinais
-    for action in actions:
-        #DELAY ANTES DE COMEÇAR
-        for x in reversed(range(10)):
-            success, image = cap.read()
-            cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
-            cv2.putText(image, f'Em {x} segundos: {action}', (15, 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+                # Loop de sinais
+                for action in actions:
+                    #DELAY ANTES DE COMEÇAR A ENSINAR UM NOVO SINAL
+                    for x in reversed(range(10)):
+                        success, image = cap.read()
+                        cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+                        cv2.putText(image, f'Em {x} segundos: {action}', (15, 20),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
-            cv2.imshow("Captura", image)
-            cv2.waitKey(1000)
+                        cv2.imshow("Captura", image)
+                        cv2.waitKey(1000)
 
-        # Loop de amostras ("Vídeos")
-        no_sequences_rh_v = int(no_sequences) #sequencias das amostras invertidas na horizontal
+                    # Loop de amostras ("Vídeos")
+                    no_sequences_rh_v = int(no_sequences) + int(nro_amostra_inicial) #sequencias das amostras invertidas na horizontal
+                    no_sequences_h_rv = int(no_sequences) + no_sequences_rh_v #sequencias das amostras invertidas na horizontal
+                    no_sequences_rh_rv = int(no_sequences) + no_sequences_h_rv #sequencias das amostras invertidas na horizontal
 
-        for sequence in range(int(no_sequences)):
-            print(sequence)
-            print(no_sequences_rh_v)
-            print("")
+                    for sequence in range(int(nro_amostra_inicial),(int(nro_amostra_inicial) + int(no_sequences))):
+                        # Loop de frames
+                        for frame_num in range(sequence_length):
+                            # Coletando o conteúdo capturado pela câmera
+                            # Frame é a imagem capturada em si
+                            # success é só um boooleano que indica se a câmera foi detectada com sucesso ou não
+                            # o read() precisa está dentro desse for para fazer uma captura por frame_num de cada sequence de cada action
+                            success, frame = cap.read()
 
-            # Loop de frames
-            for frame_num in range(sequence_length):
-                # Coletando o conteúdo capturado pela câmera
-                # Frame é a imagem capturada em si
-                # success é só um boooleano que indica se a câmera foi detectada com sucesso ou não
-                # o read() precisa está dentro desse for para fazer uma captura por frame_num de cada sequence de cada action
-                success, frame = cap.read()
+                            # Parando o código caso a câmera não foi detectada
+                            if not success:
+                                print("Câmera não detectada!")
+                                exit()
 
-                # Parando o código caso a câmera não foi detectada
-                if not success:
-                    print("Câmera não detectada!")
-                    exit()
+                            # Fazendo detecções dos pontos
+                            image_h_v, results_h_v = md.mediapipe_detection(frame, holistic) #amostras originais
+                            image_rh_v, results_rh_v = md.mediapipe_detection(cv2.flip(frame, 1), holistic_rh_v) #amostras invertidas na horizontal
+                            image_h_rv, results_h_rv = md.mediapipe_detection(cv2.flip(frame, 0), holistic_h_rv) #amostras invertidas na vertical
+                            image_rh_rv, results_rh_rv = md.mediapipe_detection(cv2.flip(frame, -1), holistic_rh_rv) #amostras invertidas nos dois eixos
 
-                # Fazendo detecções dos pontos
-                image_h_v, results_h_v = md.mediapipe_detection(frame, holistic) #amostras originais
-                image_rh_v, results_rh_v = md.mediapipe_detection(cv2.flip(frame, 1), holistic) #amostras invertidas na horizontal
+                            # Inserindo pontos na imagem
+                            dl.draw_landmarks(image_h_v, results_h_v)
 
-                # Inserindo pontos na imagem
-                dl.draw_landmarks(image_rh_v, results_rh_v)
+                            # Legendas que indicam qual amostra e gesto está sendo coletado
+                            # UTIL PARA SABER QUANDO COMEÇA E QUANDO TERMINA CADA COLETA
+                            if frame_num == 0:
+                                cv2.rectangle(image_h_v, (0, 0), (640, 40), (245, 117, 16), -1)
+                                cv2.putText(image_h_v, 'INICIANDO COLETA', (120, 200),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+                                cv2.putText(image_h_v, f'Demonstrando "{action}" -- amostra numero {sequence} -- {frame_num}', (15, 20),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+                                cv2.waitKey(
+                                    2000)  # Mandando esperar para ter tempo de você voltar pra posição original antes de começar a próxima coleta
+                            else:
+                                cv2.rectangle(image_h_v, (0, 0), (640, 40), (245, 117, 16), -1)
+                                cv2.putText(image_h_v, f'Demonstrando "{action}" -- amostra numero {sequence} -- {frame_num}', (15, 20),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
-                # Legendas que indicam qual amostra e gesto está sendo coletado
-                # UTIL PARA SABER QUANDO COMEÇA E QUANDO TERMINA CADA COLETA
-                if frame_num == 0:
-                    cv2.rectangle(image_rh_v, (0, 0), (640, 40), (245, 117, 16), -1)
-                    cv2.putText(image_rh_v, 'INICIANDO COLETA', (120, 200),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-                    cv2.putText(image_rh_v, f'Demonstrando "{action}" -- amostra numero {sequence} -- {frame_num}', (15, 20),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-                    cv2.waitKey(
-                        2000)  # Mandando esperar para ter tempo de você voltar pra posição original antes de começar a próxima coleta
-                else:
-                    cv2.rectangle(image_rh_v, (0, 0), (640, 40), (245, 117, 16), -1)
-                    cv2.putText(image_rh_v, f'Demonstrando "{action}" -- amostra numero {sequence} -- {frame_num}', (15, 20),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+                            # Montando array de coordenadas
+                            keypoints_h_v = ek.extract_keypoints(results_h_v) #amostras originais
+                            keypoints_rh_v = ek.extract_keypoints(results_rh_v) #amostras invertidas na horizontal
+                            keypoints_h_rv = ek.extract_keypoints(results_h_rv) #amostras invertidas na vertical
+                            keypoints_rh_rv = ek.extract_keypoints(results_rh_rv) #amostras invertidas nos dois eixos
 
-                # Montando array de coordenadas
-                keypoints_h_v = ek.extract_keypoints(results_h_v) #amostras originais
-                keypoints_rh_v = ek.extract_keypoints(results_rh_v) #amostras invertidas na horizontal
+                            # Pasta onde o frame será salvo
+                            npy_path_h_v = os.path.join(DATA_PATH, action, str(sequence),str(frame_num)) #amostras originais
+                            npy_path_rh_v = os.path.join(DATA_PATH, action, str(no_sequences_rh_v), str(frame_num)) #amostras invertidas na horizontal
+                            npy_path_h_rv = os.path.join(DATA_PATH, action, str(no_sequences_h_rv), str(frame_num)) #amostras invertidas na vertical
+                            npy_path_rh_rv = os.path.join(DATA_PATH, action, str(no_sequences_rh_rv), str(frame_num)) #amostras invertidas nos dois eixos
 
-                # Pasta onde o frame será salvo
-                npy_path_h_v = os.path.join(DATA_PATH, action, str(sequence),str(frame_num)) #amostras originais
-                npy_path_rh_v = os.path.join(DATA_PATH, action, str(no_sequences_rh_v), str(frame_num)) #amostras invertidas na horizontal
+                            #Salvando pontos
+                            np.save(npy_path_h_v, keypoints_h_v) #amostras originais
+                            np.save(npy_path_rh_v, keypoints_rh_v) #amostras invertidas na horizontal
+                            np.save(npy_path_h_rv, keypoints_h_rv) #amostras invertidas na vertical
+                            np.save(npy_path_rh_rv, keypoints_rh_rv) #amostras invertidas nos dois eixos
 
-                #Salvando pontos
-                #np.save(npy_path_h_v, keypoints_h_v) #amostras originais
-                #np.save(npy_path_rh_v, keypoints_rh_v) #amostras invertidas na horizontal
+                            # Esse é o comando que mostra na tela o frame capturado
+                            # "Captura" é o nome da telinha que abre
+                            # Essa telinha trava caso o loop for interrompido
+                            # Sempre que for usar esse comando parece ser bom fazer dentro de um loop, mesmo que seja exibido uma imagem fixa
+                            # TESTADO: Mesmo que aja outro loop após o termino desse a telinha também para de responder
+                            # A janela com imagem travar não é culpa do término da execução do código em si e sim do término do loop
 
-                # Esse é o comando que mostra na tela o frame capturado
-                # "Captura" é o nome da telinha que abre
-                # Essa telinha trava caso o loop for interrompido
-                # Sempre que for usar esse comando parece ser bom fazer dentro de um loop, mesmo que seja exibido uma imagem fixa
-                # TESTADO: Mesmo que aja outro loop após o termino desse a telinha também para de responder
-                # A janela com imagem travar não é culpa do término da execução do código em si e sim do término do loop
+                            cv2.imshow("Captura", image_h_v)
 
-                cv2.imshow("Captura", image_rh_v)
+                            # Encerra o loop quando é apertado a tecla "q"
+                            if cv2.waitKey(10) & 0xFF == ord('q'):
+                                break
 
-                # Encerra o loop quando é apertado a tecla "q"
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
+                        # Aumentando no_sequences das amostras viradas
+                        no_sequences_rh_v = no_sequences_rh_v + 1  # amostras invertidas na horizontal
+                        no_sequences_h_rv = no_sequences_h_rv + 1  # amostras invertidas na vertical
+                        no_sequences_rh_rv = no_sequences_rh_rv + 1  # amostras invertidas nos dois eixos
 
-            # Aumentando no_sequences das amostras viradas
-            no_sequences_rh_v = no_sequences_rh_v + 1  # amostras invertidas na horizontal
-
-    cap.release()  # Para de monitorar a câmera
-    cv2.destroyAllWindows()  # Fecha todas as telas do cv2.imshow(), mesmo que elas não estejam respondendo
+                cap.release()  # Para de monitorar a câmera
+                cv2.destroyAllWindows()  # Fecha todas as telas do cv2.imshow(), mesmo que elas não estejam respondendo
