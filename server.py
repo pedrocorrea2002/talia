6a902a9c2fa53d864ab6fb3792ea8050d6c8d8ae
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-11-19 16:07:38 -0300

    Inicio da criação do back do formulário de login

--- server.py
+++ server.py
@@ -1,13 +1,28 @@
 # unique id using Date.now() do javascript
 
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
+import time
 
 abacate = Flask(__name__)
-abacate.config["ATUM"] = "BALEIA"
+abacate.config['SECRET_KEY'] = str(time.time())
+
+class UserRegistration(FlaskForm):
+    username = StringField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
+    password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
+    confirm_password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
+    situation = ""
+
 
 @abacate.route("/home")
 def Main():
-    return render_template("home.html")
+    logon_form = UserRegistration()
+    return render_template("home.html", form=logon_form)
+
+def Logon():
+    return 
 
 if __name__ == '__main__':
     abacate.run(debug=True)
