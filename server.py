c14fda177346067fdf7c45cbb557838b95328e3e
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-11-20 00:37:26 -0300

    validação no login se o usuário existe

--- server.py
+++ server.py
@@ -3,7 +3,8 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, equal_to, ValidationError
+from wtforms.validators import InputRequired, EqualTo
+from flask_login import login_user
 import time
 import os
 
@@ -14,16 +15,20 @@ class UserRegistration(FlaskForm):
     username = StringField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
     password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
     confirm_password = PasswordField(label="", validators=[
+        EqualTo('password',message="As duas senhas precisam ser iguais!"),
         InputRequired(message="Preenchimento obrigatório!")
-        # equal_to(password,"As duas senhas precisam ser iguais!")
     ])
 
 class UserAuthentication(FlaskForm):
     username = StringField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
     password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
-    submit = SubmitField('submit_logon')
 
-@abacate.route("/home", methods=['GET','POST'])
+class user:
+    def __init__(self, username,password):
+        self.username = username
+        self.password = password
+
+@abacate.route("/home", methods=['GET','POST'], )
 def home():
     logon_form = UserRegistration()
     login_form = UserAuthentication()
@@ -38,15 +43,16 @@ def home():
         user_folder = os.path.join("users",f"{logon_form.username.data}_{logon_form.password.data}")
 
         if(os.path.exists(os.path.join(user_folder))):
-            logon_form.username.
-            # logon_form.username.post_validate(logon_form, ValidationError(message="Já existe um usuário com esse nome e senha cadastrado!"))
+            return render_template("home.html", form_logon=logon_form, form_login=login_form, exist_user=True)
         else:
             os.mkdir(user_folder)
+            return render_template("what_sample.html", user=user(logon_form.username.data,logon_form.password.data))
 
     elif request.method == 'POST' and request.form['button'] == "login"  and login_form.validate_on_submit():
         pass
 
-    return render_template("home.html", form_logon=logon_form, form_login=login_form)
+    return render_template("home.html", form_logon=logon_form, form_login=login_form, exist_user=False)
+
 
 if __name__ == '__main__':
     abacate.run(debug=True)
