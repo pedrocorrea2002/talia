b00c74e2ad6a7b7bca2a20156bf35182cb476175
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-11-19 23:10:35 -0300

    criando validções no formulário

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 # unique id using Date.now() do javascript
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, equal_to, ValidationError
 import time
+import os
 
 abacate = Flask(__name__)
 abacate.config['SECRET_KEY'] = str(time.time())
@@ -12,17 +13,40 @@ abacate.config['SECRET_KEY'] = str(time.time())
 class UserRegistration(FlaskForm):
     username = StringField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
     password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
-    confirm_password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
-    situation = ""
+    confirm_password = PasswordField(label="", validators=[
+        InputRequired(message="Preenchimento obrigatório!")
+        # equal_to(password,"As duas senhas precisam ser iguais!")
+    ])
 
+class UserAuthentication(FlaskForm):
+    username = StringField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
+    password = PasswordField(label="", validators=[InputRequired(message="Preenchimento obrigatório!")])
+    submit = SubmitField('submit_logon')
 
-@abacate.route("/home")
-def Main():
+@abacate.route("/home", methods=['GET','POST'])
+def home():
     logon_form = UserRegistration()
-    return render_template("home.html", form=logon_form)
+    login_form = UserAuthentication()
 
-def Logon():
-    return 
+    print("button: " + request.form['button'])
+    print("validade start")
+    print(logon_form.validate_on_submit())
+    print(logon_form.errors)
+    print("validade end")
+
+    if request.method == 'POST' and request.form['button'] == "logon"  and logon_form.validate_on_submit():
+        user_folder = os.path.join("users",f"{logon_form.username.data}_{logon_form.password.data}")
+
+        if(os.path.exists(os.path.join(user_folder))):
+            logon_form.username.
+            # logon_form.username.post_validate(logon_form, ValidationError(message="Já existe um usuário com esse nome e senha cadastrado!"))
+        else:
+            os.mkdir(user_folder)
+
+    elif request.method == 'POST' and request.form['button'] == "login"  and login_form.validate_on_submit():
+        pass
+
+    return render_template("home.html", form_logon=logon_form, form_login=login_form)
 
 if __name__ == '__main__':
     abacate.run(debug=True)
