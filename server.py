3cd245c628f227dad3b20d05832f6abf138e389e
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-11-20 22:59:11 -0300

    comentários

--- server.py
+++ server.py
@@ -35,13 +35,13 @@ class user:
         self.username = username
         self.password = password
 
-    def is_active(self):
+    def is_active(self): #? PODERIA SER MAIS COMPLEXO, MAS EU FORCEI COMO TRUE, ESSA FUNÇÃO É CHAMADA PELO PRÓPRIO LoginManager DO flask_login
         return True
     
     def get_id(self):
         return (self.username,self.password)
     
-    def is_authenticated(self):
+    def is_authenticated(self): #? PODERIA SER MAIS COMPLEXO, MAS EU FORCEI COMO TRUE, ESSA FUNÇÃO É CHAMADA PELO PRÓPRIO LoginManager DO flask_login
         return True
 
 @abacate.route("/")
