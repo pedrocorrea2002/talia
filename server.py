86d18907bfaf4b5a33fcb3b9e4c62a092f076119
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-12-18 01:45:28 -0300

    teste2

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask_login import login_user, current_user, login_required, LoginManager
 
 from static.utils.classes.userAuth_classes import UserAuthentication, UserRegistration
 from static.utils.classes.user import user
-from static.utils.functions.sinais_translator import sinais_translator
+# from static.utils.functions.sinais_translator import sinais_translator
 
 
 abacate = Flask(__name__)
